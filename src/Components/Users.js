82ae45850129444cdab215b3769e89b1fa10a2b0
mayank dutta <smayankdutt@gmail.com>
2023-03-14 03:16:47 +0530

    initials fixed

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-const link = process.env.REACT_APP_LINK
+const link = process.env.REACT_APP_LINK;
 
 const Users = () => {
-
   const [users, setUsers] = React.useState();
 
   var id = 0;
 
   React.useEffect(() => {
-
     const getUsers = async () => {
-      await axios.get(`${link}/users`)
-        .then(res => setUsers(res.data))
-        .catch(err => console.error(err));
-    }
+      await axios
+        .get(`${link}/users`)
+        .then((res) => setUsers(res.data))
+        .catch((err) => console.error(err));
+    };
 
     getUsers();
-
-  }, [])
+  }, []);
 
   return (
-    <div style={{margin : '3em'}}>
+    <div style={{ margin: "3em" }}>
       <h1>Users List </h1>
-      <table className='table'>
+      <table className="table">
         <thead>
           <tr>
             <th scope="col">Name</th>
@@ -32,21 +30,20 @@ const Users = () => {
           </tr>
         </thead>
         <tbody>
-            {
-              users ? users.map(user => {
+          {users
+            ? users.map((user) => {
                 return (
                   <tr>
                     <td>{user.name}</td>
                     <td>{user.email}</td>
                   </tr>
-                )
-              }) : "Loading..."
-            }
+                );
+              })
+            : "Loading..."}
         </tbody>
       </table>
-
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
